f95474153ca40a4ce529e0b55b3e9b58d3f19503
Sharon Kim <21800087@handong.edu>
2019-05-28 21:54:35 +0900

    changed Utils.java

--- src/main/java/edu/handong/analysis/utils/Utils.java
+++ src/main/java/edu/handong/analysis/utils/Utils.java
@@ -1,34 +1,33 @@
 package edu.handong.analysis.utils;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Utils {
-	public static ArrayList<String> getLines(String file, boolean removerHeader){
-		ArrayList<String> lines = new ArrayList<String>();
-		while(!removerHeader) {
-			BufferedInputStream inputStream= new BufferedInputStream(new FileInputStream(new File(file)));
-	        while (inputStream.read()!=-1){
-	        	 
-	        }
-			
+	public static ArrayList<String> getLines(String file, boolean removerHeader) throws IOException{
+		BufferedReader inputStream= new BufferedReader(new FileReader(file));
+		FileWriter writer;
+        while (inputStream.read()!=-1){
+        	writer = new FileWriter(file, removerHeader);
+        }
+        ArrayList<String> lines = new ArrayList<String>();
+        while(writer.hasNext()) {
+        	lines.add(writer.next());
+        }
 		return lines;
 	}
 	
 	public static void writeAFile(ArrayList<String> lines, String targetFileName) {
-		try {
-            Path path = ;
-            Files.createDirectories(path.getParent());
-
-            Files.write(path, "Log log".getBytes());
-
-            System.out.println(Files.readAllLines(path));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		BufferedReader inputStream= new BufferedReader(new FileReader(file));
+        while (inputStream.read()!=-1){}
 	}
 }
